2a2c6bf1ecb811f7ade2cef1e29111e798d95d50
md sameer <md477959@gmail.com>
2024-07-19 15:51:12 +0530

    data transformation

--- app.py
+++ app.py
@@ -8,13 +8,20 @@ from mlproject.logger import logging
 from mlproject.exception import CustomException
 from mlproject.components.data_ingestion import DataIngestion
 from mlproject.components.data_ingestion import DataIngestionConfig
-
+from mlproject.components.data_transformation import DataTransformationConfig,DataTransformation
 if __name__ == "__main__":
     logging.info("The execution has started")
 
     try:
+        # data_ingestion=DataIngestion()
+        # data_ingestion.initiate_data_ingestion()
         data_ingestion=DataIngestion()
-        data_ingestion.initiate_data_ingestion()
+        train_data_path,test_data_path=data_ingestion.initiate_data_ingestion()
+
+        #data_transformation_config=DataTransformationConfig()
+        data_transformation=DataTransformation()
+        train_arr,test_arr,_=data_transformation.initiate_data_transormation(train_data_path,test_data_path)
+
 
     except Exception as e:
         logging.info("Custom Exception")
--- src/mlproject/utils.py
+++ src/mlproject/utils.py
@@ -8,6 +8,8 @@ from mlproject.exception import CustomException
 import pandas as pd
 from dotenv import load_dotenv
 
+import pickle
+import numpy as np
 import pymysql
 
 load_dotenv()
@@ -33,3 +35,14 @@ def read_sql_data():
         return df
     except Exception as e:
         raise CustomException(e)
+def save_object(file_path, obj):
+    try:
+        dir_path = os.path.dirname(file_path)
+
+        os.makedirs(dir_path, exist_ok=True)
+
+        with open(file_path, "wb") as file_obj:
+            pickle.dump(obj, file_obj)
+
+    except Exception as e:
+        raise CustomException(e, sys)
